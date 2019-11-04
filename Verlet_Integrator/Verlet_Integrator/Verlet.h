c46f87662561642196ee5b4783f09c4570847aca
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-11-04 11:15:27 +0100

    first version

--- Verlet_Integrator/Verlet_Integrator/Verlet.h
+++ Verlet_Integrator/Verlet_Integrator/Verlet.h
@@ -1,7 +1,28 @@
 #ifndef _VERLET_H
 #define _VERLET_H
+#include <iostream>
+using namespace std;
 
+float Verlet_Integration(float xi, float& xo, float ai, float dt) {
+	
+	float x_new;
 
+	float v_new;
+
+	float a_new;
+
+	x_new = xi + (xi - xo) + ai * dt * dt;
+
+	v_new = (x_new - xi) / dt;
+
+	a_new = ai;
+
+	cout << "x: " << x_new << " v: " << v_new << " a: " << a_new << endl;
+	
+	xo = xi;
+
+	return x_new;
+}
 
 #endif // !_VERLET_H
 
--- Verlet_Integrator/Verlet_Integrator/main.cpp
+++ Verlet_Integrator/Verlet_Integrator/main.cpp
@@ -1,5 +1,34 @@
 #include "Verlet.h"
 
 void main() {
+	float x, x_old;
+	float v;
+	float a = 0;
+	float dt = 1;
 
+	cout << "Case dt: " << dt << " and a: " << a << endl;
+	x_old = 0;
+	x = Verlet_Integration(5, x_old, a, dt);
+	while (x <=50)
+	{
+		x = Verlet_Integration(x, x_old, a, dt);
+	}
+
+	a = 1;
+	cout << "Case dt: " << dt << " and a: " << a << endl;
+	x_old = 0;
+	x = Verlet_Integration(5, x_old, a, dt);
+	while (x <= 50)
+	{
+		x = Verlet_Integration(x, x_old, a, dt);
+	}
+
+	dt = 0.3;
+	cout << "Case dt: " << dt << " and a: " << a << endl;
+	x_old = 0;
+	x = Verlet_Integration(5, x_old, a, dt);
+	while (x <= 50)
+	{
+		x = Verlet_Integration(x, x_old, a, dt);
+	}
 }
