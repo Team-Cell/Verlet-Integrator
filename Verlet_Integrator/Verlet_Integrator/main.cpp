3fc8da223f835d3cbf94e0bb3eea4d3243dd9bce
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-11-04 12:02:08 +0100

    endl

--- Verlet_Integrator/Verlet_Integrator/main.cpp
+++ Verlet_Integrator/Verlet_Integrator/main.cpp
@@ -13,6 +13,7 @@ void main() {
 	{
 		x = Verlet_Integration(x, x_old, a, dt);
 	}
+	cout << endl;
 
 	a = 1;
 	cout << "Case dt: " << dt << " and a: " << a << endl;
@@ -22,8 +23,9 @@ void main() {
 	{
 		x = Verlet_Integration(x, x_old, a, dt);
 	}
+	cout <<endl;
 
-	dt = 0.3;
+	dt = 0.5;
 	cout << "Case dt: " << dt << " and a: " << a << endl;
 	x_old = 0;
 	x = Verlet_Integration(5, x_old, a, dt);
