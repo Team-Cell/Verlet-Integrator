f35f7e1e1fe406713d65b2610a1e87624b9ebc66
Arnau77 <47742576+Arnau77@users.noreply.github.com>
2019-11-09 14:13:59 +0100

    calculate acceleration in verlet integrator

--- Verlet_Integrator/Verlet_Integrator/Verlet.h
+++ Verlet_Integrator/Verlet_Integrator/Verlet.h
@@ -24,28 +24,52 @@ float Verlet_Integration(float xi, float& xo, float ai, float dt) {
 
 // TODO 1: 
 //Make a function which returns the velocity Verlet
-float Verlet_Velocity(float vi, float ai, float ao, float dt) {
+float Verlet_Velocity(float vi, float ai, float a_new, float dt) {
 
 	float v_new;
 
-	v_new = vi + ((ai + ao) / 2) * dt;
+	v_new = vi + ((ai + a_new) / 2) * dt;
+
+	cout << " v: " << v_new << " a: " << a_new << endl;
 
 	return v_new;
 }
 
-float Verlet_Velocity(float xi, float& x_new, float vi, float ai, float ao, float dt) {
+float Verlet_Velocity(float xi, float& x_new, float vi, float ai, float a_new, float dt) {
 
 	float v_new;
 
 	x_new = xi + vi * dt + 0.5 * ai * dt * dt;
 
-	v_new = vi + ((ai + ao) / 2) * dt;
+	v_new = vi + ((ai + a_new) / 2) * dt;
+
+	cout << "x: " << x_new << " v: " << v_new << " a: " << a_new << endl;
 
 	return v_new;
 }
 
 //TODO 2:
 //Make a function which returns the acceleration 
+
+float Verlet_Acceleration(float fi, float m) {
+	
+	float a_new;
+
+	a_new = fi / m;
+
+	return a_new;
+}
+
+float Verlet_Acceleration(float fi, float fi2, float m) {
+
+	float a_new, f_total;
+
+	f_total = fi + fi2;
+
+	a_new = f_total / m;
+
+	return a_new;
+}
 
 //Other functions which return different values depending on the variables we have
 
